e0fbf8fd02d87405d68a56575970888668f2cae9
Erxiangbo-bo <83910430+Erxiangbo-bo@users.noreply.github.com>
2023-04-12 21:17:16 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ACDCTestSystemPart1.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace ACDCTestSystemPart1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<LoginPage>("LoginPage");
+            containerRegistry.RegisterForNavigation<TestWindow>("TestWindow");
+            containerRegistry.RegisterForNavigation<ConfigWindow>("ConfigWindow");
+        }
+    }
+}
---
+++ TestRecord.sql
@@ -0,0 +1,12 @@
+create table TestRecord
+(
+ID int identity(1,1) primary key,
+BoardID varchar(50),
+Result1 varchar(20),
+Result2 varchar(20),
+Result3 varchar(20),
+Result4 varchar(20),
+Result5 varchar(20),
+Maintain varchar(50),
+FPY varchar(50)
+)
